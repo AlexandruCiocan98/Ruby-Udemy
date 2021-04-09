92c2cd310b7f4fbe8a67dc980a31cdc58391c297
Alexandru Ciocan <Alexandru@Alexandrus-MacBook.local>
2021-04-09 12:20:46 +0300

    first commit

---
+++ comments.rb
@@ -0,0 +1,23 @@
+#asdfsddsaaaassdlklakjflkcmnxz
+#sakdlkvlkznx
+#asldkfjalfnasd
+#lkzxvlkjf
+
+# Method one
+#Use command + / to comment out everything selected or 
+#Just the line i am in. Same command to uncomment
+
+# Method two
+=begin
+
+;ldfaksdjflkadf
+lkasdlkfjla
+lkasldkfj
+
+=end
+
+#use =begin and =end to comment out a block of code
+
+
+
+
